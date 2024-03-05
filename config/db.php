96bdaae4b75163cfeed2782e32a339c2b33ffedf
athlioplasma <sb3e@mail.ru>
2024-03-05 13:52:39 +0700

    Интеграция Middleware

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'ecbkfpeg_m3',
+    'username' => 'ecbkfpeg',
+    'password' => '4T6reg',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
